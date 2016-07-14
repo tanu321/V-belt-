a0d725b94906479504db7e52f7211792a1236695
sukhbir321 <sukhbirkour7272@gmail.com>
2016-07-14 12:54:59 +0530

    done

--- V-beltpulley/client/main.js
+++ V-beltpulley/client/main.js
@@ -1,18 +1,11 @@
 $('.carousel').carousel({
   interval: 5000
-})
-Template.product.events({
-'click .btn'(event){
-event.preventDefault();
-var a = document.getElementsByClassName("prc")[0].value; 
-console.log(a);
-if(!a){
-console.log("enter a number");
-}
+});
 
-else
-{
-console.log("submit");
+Template.product.events({
+'click button'(events){
+
+var a=$( "#myselect" ).val();
+console.log(a);
 }
-},
 });
---
+++ V-beltpulley/collection/collection.js
@@ -0,0 +1,1 @@
+Pulley = new Mongo.Collection('type');
